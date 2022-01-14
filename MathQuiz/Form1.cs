83332df309945b87c240ca7f5d893d3a9cd5dabe
ridaelbardai <rb.99@hotmail.fr>
2022-01-14 18:36:00 +0100

    initial commit

--- MathQuiz/Form1.cs
+++ MathQuiz/Form1.cs
@@ -12,9 +12,51 @@ namespace MathQuiz
 {
     public partial class Form1 : Form
     {
+        // Create a Random object called randomizer 
+        // to generate random numbers.
+        Random randomizer = new Random();
+
+
+        /// <summary>
+        /// Start the quiz by filling in all of the problems
+        /// and starting the timer.
+        /// </summary>
+        public void StartTheQuiz()
+        {
+
+            // Convert the two randomly generated numbers
+            // into strings so that they can be displayed
+            // in the label controls.
+            plusLeftLabel.Text = randomizer.Next(51).ToString();
+         
+            plusRightLabel.Text = randomizer.Next(51).ToString();
+
+            // 'sum' is the name of the NumericUpDown control.
+            // This step makes sure its value is zero before
+            // adding any values to it.
+            sum.Value = 0;
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
         }
     }
 }
